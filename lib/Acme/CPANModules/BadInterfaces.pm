9b33723993810bd5d3d49523f509b05c117dfb83
perlancar (on pc-office) <perlancar@gmail.com>
2019-03-13 20:32:45 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/BadInterfaces.pm
@@ -0,0 +1,25 @@
+package Acme::CPANModules::BadInterfaces;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => 'Modules with bad interfaces',
+    description => <<'_',
+
+This is my subjective list of modules that I think need to be highlighted as
+examples of those that have {bad,awful,kludgy} interface (API), making them
+unnecessarily inconvenient to use. Modules that leave you in a bad taste or even
+makes you hate Perl and programming because of them.
+
+_
+    entries => [
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 append:SEE ALSO
+
+L<Acme::CPANModules::GoodInterfaces>
